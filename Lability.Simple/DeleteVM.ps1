ab9a203f65f94b602297024584604efbe348b57c
Axel Lund <axel96@live.no>
2020-09-23 14:10:33 +0200

    added silentlycontinue to shutdown of vm

--- Lability.Simple/DeleteVM.ps1
+++ Lability.Simple/DeleteVM.ps1
@@ -1,5 +1,5 @@
 #Requires -RunAsAdministrator 
 $VMName = Read-Host -Prompt "Enter hostname of VM to delete"
-Stop-VM -Name $VMName -TurnOff -Force
+Stop-VM -Name $VMName -TurnOff -Force -ErrorAction SilentlyContinue
 Remove-VM -Name $VMName -Force
 Remove-Item -Path "${env:LabilityDifferencingVhdPath}\${VMName}*" -ErrorAction SilentlyContinue -Force
