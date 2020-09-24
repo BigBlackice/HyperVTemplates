aea09463b03ccabb92634b9650b5eb5888c61464
github-nilsnilson <nilsnilson420@gmail.com>
2020-09-24 15:51:44 +0200

    bugfix

--- LabTemplates/ConfigScript.ps1
+++ LabTemplates/ConfigScript.ps1
@@ -81,8 +81,9 @@ Configuration TemplateConfig {
         xComputer 'Hostname' {
             Name = $node.NodeName;
         }
-        Set-VMDvdDrive -vmname  "PFSENSE1" -Path "C:\Lability\ISOs\pfSense-CE-2.4.5-RELEASE-amd64.iso";
+        
 
     }
 
 }
+Set-VMDvdDrive -vmname  "PFSENSE1" -Path "C:\Lability\ISOs\pfSense-CE-2.4.5-RELEASE-amd64.iso";
